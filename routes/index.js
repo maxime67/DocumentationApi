51930256df346ab5214c3eb882ff8b94c3fb4a66
maxime67 <maxime.heim3@gmail.com>
2025-01-23 20:24:30 +0100

    update

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const { MongoClient, ObjectId} = require('mongodb');
-const { body,validationResult } = require('express-validator');
+const { MongoClient} = require('mongodb');
 
 require('dotenv').config();
 
